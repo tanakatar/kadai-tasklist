01707c2f65d8e6f48aafbf2d104e961b6043051b
h.n <tt2322778@gmail.com>
2020-08-26 13:48:36 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -18,17 +19,15 @@ class TasksController < ApplicationController
       flash[:success] = "1件登録しました"
       redirect_to @task
     else
-      flash[:danger] = "登録できませんでした"
+      flash.now[:danger] = "登録できませんでした"
       render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = "更新されました"
       redirect_to @task
@@ -39,7 +38,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "削除されました"
@@ -47,7 +45,12 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end

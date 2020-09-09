ed5c7e480eea5621afa82498bb52ef1cd40a1a33
h.n <tt2322778@gmail.com>
2020-09-09 12:37:39 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :correct_user, only: [:show, :destroy]
+  before_action :require_user_logged_in
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
   
   def show
@@ -13,11 +14,10 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "1件登録しました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "登録できませんでした"
       render :new
@@ -39,7 +39,6 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = "削除されました"
     redirect_to tasks_url
   end
@@ -52,5 +51,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_save { self.name.downcase! }
-  validates :name, presence: true, length: { maximum: 30 },
+  validates :name, presence: true, length: { maximum: 50 },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :tasks
 end

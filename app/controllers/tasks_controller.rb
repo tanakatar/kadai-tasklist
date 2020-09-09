5df13031123d0d72c1f4721745b240f0e7feda47
h.n <tt2322778@gmail.com>
2020-09-09 13:20:53 +0000

    task-heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :destroy]
   before_action :require_user_logged_in
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
